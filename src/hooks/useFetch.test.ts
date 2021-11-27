9e02bcb0d66f815945b8bc6d2188fb4de6165c8d
Isaiah <computerblender@gmail.com>
2021-11-27 01:11:32 -0700

    Add several additional test-cases for useFetch
    - should set error state appropriately
    - should parse non-JSON response
    - should not fetch data if ref is falsy
    
    TODO:
    - should not throw if ref becomes false during fetch
    - should provide response properties
    (Latter might be challenging, unless I pass the un-modified Response
    object instead of my "Parsed" Response object.)

--- src/hooks/useFetch.test.ts
+++ src/hooks/useFetch.test.ts
@@ -8,14 +8,17 @@ const stubbedData = [
   { name: 'Italy', capital: 'Rome' },
 ];
 
-const stubbedFetchUrl = 'api/countries-url-mocked';
+const url = 'api/demo-url';
 
 describe('useFetch()', () => {
-  it('should return data after fetch', async () => {
-    fetchMock.mock(stubbedFetchUrl, JSON.stringify(stubbedData));
+  afterEach(() => {
+    fetchMock.restore();
+  });
 
+  it('should return data after fetch', async () => {
+    fetchMock.mock(url, JSON.stringify(stubbedData));
     const { result, waitForNextUpdate } = renderHook(() =>
-      useFetch(stubbedFetchUrl, { current: true }, {}),
+      useFetch(url, { current: true }, {}),
     );
 
     await waitForNextUpdate();
@@ -23,5 +26,61 @@ describe('useFetch()', () => {
     expect(result.current.isLoading).to.be.false;
     expect(result.current.error).to.be.null;
     expect(result.current.response?.body).to.deep.equal(stubbedData);
+  });
+
+  it('should set error state appropriately and set isLoading to False', async () => {
+    const errMsg = 'Oops!';
+
+    fetchMock.mock(
+      'error-url',
+      new Promise(() => {
+        throw errMsg;
+      }),
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetch('error-url', { current: true }, {}),
+    );
+
+    await waitForNextUpdate();
+
+    expect(result.current.isLoading).to.be.false;
+    expect(result.current.response).to.be.null;
+    expect(result.current.error).to.be.equal(errMsg);
+  });
+
+  it("should be able to parse body even if it isn't JSON", async () => {
+    const body = 'Unauthorized, for example';
+    const url = 'text-response-url';
+
+    fetchMock.mock(url, body);
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetch(url, { current: true }, {}),
+    );
+
+    await waitForNextUpdate();
+
+    expect(result.current.response?.body).to.be.equal(body);
+  });
+
+  it('should not fetch data if current ref is falsy', async () => {
+    const url = 'should-not-be-called';
+    fetchMock.mock(url, 'res');
+    const { result } = renderHook(() => useFetch(url, { current: false }, {}));
+
+    expect(fetchMock.called(url)).to.be.false;
+    expect(result.current).to.be.deep.equal({
+      isLoading: true,
+      response: null,
+      error: null,
+    });
+  });
+
+  xit('should not throw an error if ref is unmounted before fetch returns', () => {});
+  xit('should set isLoading to True if no response or error are received', () => {});
+
+  xit('should provide Response properties (eg, ok, statusCode, etc.)', () => {
+    // NOTE: not sure how feasible this is with current mocking lib
   });
 });
