fecc94a7e9cf719ef2144dacdee5bde6d7cac6e5
Isaiah <computerblender@gmail.com>
2021-11-27 21:22:50 -0700

    Implement additional tests for BlogPage
    Also, refactor the component.
    
    Currently does not include tests for the Comment portions.

--- src/pages/BlogPage.test.tsx
+++ src/pages/BlogPage.test.tsx
@@ -1,44 +1,85 @@
 import { screen } from '@testing-library/react';
 import { expect } from 'chai';
+import fetchMock from 'fetch-mock/esm/client';
 import * as React from 'react';
 import { Route, Routes } from 'react-router';
-import { renderWithRouter } from '../utils/testUtils';
-import BlogPage from './BlogPage';
-import fetchMock from 'fetch-mock/esm/client';
+import { DataStoreContext } from '../contexts/dataStore';
 import { SNOWPACK_PUBLIC_API_URL } from '../utils/envManager';
+import { renderWithRouter } from '../utils/testUtils';
+import { testBlog } from '../__fixtures__/APIData';
+import BlogPage from './BlogPage';
+
+function getBlogPageRoute() {
+  return (
+    <Routes>
+      <Route path="blog/:blogid" element={<BlogPage />} />
+    </Routes>
+  );
+}
+
+const nullDataStoreProviderMock = {
+  getItem: () => null,
+  setItem: () => {},
+};
+
+const getAPIRoute = (id: string) => `${SNOWPACK_PUBLIC_API_URL}/blogs/${id}`;
+const getRouterPath = (id: string) => `/blog/${id}`;
 
 describe('<BlogPage>', () => {
-  xit('Redirects to 404 page if the blog does not exist', () => {
-    const invalidId = '/invalid-blog-id';
-    const invalidRoute = SNOWPACK_PUBLIC_API_URL + '/blogs' + invalidId;
+  it('Renders a 404 error if the blog does not exist', async () => {
+    const invalidId = 'invalid-blog-id';
+    fetchMock.mock(getAPIRoute(invalidId), 404);
 
-    fetchMock.mock(invalidRoute, 404);
+    renderWithRouter(
+      <DataStoreContext.Provider value={nullDataStoreProviderMock}>
+        {getBlogPageRoute()}
+      </DataStoreContext.Provider>,
+      { route: getRouterPath(invalidId) },
+    );
 
-    renderWithRouter(<BlogPage />, { route: '/blog' + invalidId });
+    await fetchMock.flush(true);
 
-    expect(document.body.contains(screen.getByText(/404/i)));
+    expect(document.body.contains(screen.queryByText(/loading/i))).to.be.false;
+    expect(document.body.contains(screen.getByText(/404/i))).to.be.true;
   });
 
-  xit('Renders a blog post immediately if it has already been loaded', async () => {
-    // TODO: mock DataStore component
+  it('Renders a blog post immediately if it has already been loaded', async () => {
+    const dataStoreProviderMock = {
+      getItem: () => testBlog,
+      setItem: () => {},
+    };
+
     renderWithRouter(
-      <Routes>
-        <Route path="/blog/:blogid" element={<BlogPage />} />
-      </Routes>,
+      <DataStoreContext.Provider value={dataStoreProviderMock}>
+        {getBlogPageRoute()}
+      </DataStoreContext.Provider>,
+
       { route: '/blog/arbitrary-blog-id' },
     );
 
-    const title = screen.getByText(/testblog/i);
-    const content = screen.getByText('test test test');
-    const username = screen.getByText('johnny test');
+    const title = screen.getByText(testBlog.title);
+    const content = screen.getByText(testBlog.content);
+    const username = screen.getByText(testBlog.author.username);
 
     expect(document.body.contains(title));
     expect(document.body.contains(content));
     expect(document.body.contains(username));
   });
 
+  it('Fetches a blog post if it is not already loaded', async () => {
+    const blogId = 'valid-unfetched-blog-id';
+    fetchMock.mock(getAPIRoute(blogId), { body: { content: testBlog } });
+
+    renderWithRouter(
+      <DataStoreContext.Provider value={nullDataStoreProviderMock}>
+        {getBlogPageRoute()}
+      </DataStoreContext.Provider>,
+      { route: getRouterPath(blogId) },
+    );
+  });
+
+  // TODO: once I add comments...
+  xit('Renders a "no comments" box if there are no comments', () => {});
   xit('Renders a list of comments, if there are comments', () => {});
   xit('Renders a comment form if user is logged in and a blog is loaded', () => {});
-  xit('Renders a "no comments" box if there are no comments', () => {});
-  xit('Fetches a blog post if it is not already loaded', () => {});
 });
--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,26 +1,31 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router';
 import Blog from '../components/Blog';
+import Loading from '../components/Loading';
 import useGetData from '../hooks/useDataStore';
+import { SNOWPACK_PUBLIC_API_URL } from '../utils/envManager';
 import NotFound from './NotFound';
 
 export default function BlogPage() {
   const { blogid } = useParams();
-  const { data, isLoading, error } = useGetData(`blog/${blogid}`);
-
-  // TODO: "notFound" should only display after Fetch 404s
-  // otherwise, should display a Loading component
-  return data ? (
-    <article>
-      {isLoading && <div>loading...</div>}
-      {error && <div>{error as string}</div>}
-      <Blog data={data} />
-      <section>
-        <h2>Comments</h2>
-        <div>Comment ...</div>
-      </section>
-    </article>
-  ) : (
-    <NotFound />
+  const { data, isLoading, error } = useGetData(
+    `${SNOWPACK_PUBLIC_API_URL}/blogs/${blogid}`,
   );
+
+  if (isLoading) {
+    return <Loading />;
+  } else if (!data) {
+    return <NotFound />;
+  } else {
+    return (
+      <article>
+        {error && <div>{error as string}</div>}
+        <Blog data={data} />
+        <section>
+          <h2>Comments</h2>
+          <div>Comment ...</div>
+        </section>
+      </article>
+    );
+  }
 }
