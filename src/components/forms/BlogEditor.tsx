70fcfef1c7cba97f14b1d60f82144676e3070bd2
Isaiah <computerblender@gmail.com>
2021-12-13 00:40:00 -0700

    Begin implementing blog form

--- src/components/forms/BlogEditor.tsx
+++ src/components/forms/BlogEditor.tsx
@@ -1,22 +1,62 @@
+import { format } from 'date-fns';
+import { parseISO } from 'date-fns/esm';
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
 import { usePrompt } from '../../hooks/usePrompt';
-import Blog from '../Blog';
+import fetchData from '../../utils/fetchData';
 
 interface BlogEditorProps {
   blog: BlogData;
 }
 
-// How to handle "Saved" status state? ie--are there any unsaved changes?
-// react-router's Prompt/usePrompt were pulled from current release :(
-// In lieu of that, a custom "useBeforeUnload" hook might be applicable
-// https://codesandbox.io/s/pwrux
+interface Inputs {
+  title: string;
+  content: string;
+  publishDate: string;
+}
 
-// ...Or follow the react-router dev's suggestions, and find non-blocking solution
+// TODO? - find non-blocking solution for saving user's drafts
 // (ie, just save the draft in state, maybe displaying that there are unsaved changes)
-// but tbh, I think blocking is preferable UX in many cases. That draft state is
+// But tbh, blocking might be preferable UX in many cases. That draft state is
 // likely going to be considered ephemeral by the user, so a simple warning to
 // deal with it in the moment seems like a perfectly acceptable solution.
 export default function BlogEditor({ blog }: BlogEditorProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      title: blog.title,
+      content: blog.content,
+      publishDate: format(parseISO(blog.publishDate), "yyyy-MM-dd'T'hh:mm"),
+    },
+  });
+
+  const mutation = useMutation<any, unknown, Inputs, unknown>(
+    async (formData) => {
+      // return await fetchData('POST BLOG URL', {
+      //   credentials: 'include',
+      //   method: 'POST',
+      //   body: formData,
+      // });
+      console.log(formData);
+    },
+  );
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => mutation.mutate(data);
+
   usePrompt('Are you sure?', true);
-  return <Blog data={blog} />;
+  return (
+    <form name="blog-form" aria-label="form" onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register('title', { required: true })} />
+      <textarea {...register('content', { required: true })} />
+      <input
+        type="datetime-local"
+        {...register('publishDate', { required: true })}
+      />
+      <input type="submit" value="Publish" />
+    </form>
+  );
 }
